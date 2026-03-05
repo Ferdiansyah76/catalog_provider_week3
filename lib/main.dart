92d1544f2c6b10b2e36631d8fd2bdd1f85b26beb
Ferdiansyah76 <bagusferdiansyah750@gmail.com>
2026-03-05 22:49:01 +0700

    penambahan import dan provider: ^6.0.5 di pubspec.yaml dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main () {
   runApp(
@@ -100,3 +101,35 @@ class AddButton extends StatelessWidget {
 
 
 // Halaman Keranjang
+class MyCart extends StatelessWidget {
+  const MyCart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var cart = context.watch<CartModel>();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Keranjang Belanja')),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (context, index) =>
+                  ListTile(leading: const Icon(Icons.fastfood), title: Text(cart.items[index])),
+            ),
+          ),
+          const Divider(),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: ElevatedButton(
+              onPressed: () => cart.removeAll(),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red), 
+              child: const Text('Hapus Keranjang', style: TextStyle(color: Colors.white)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
