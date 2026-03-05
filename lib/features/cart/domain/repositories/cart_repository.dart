938ddfbe25a17374cf0436805f47195be1d22643
Ferdiansyah76 <bagusferdiansyah750@gmail.com>
2026-03-05 23:28:36 +0700

    implementasi file cart_repository.dart

---
+++ lib/features/cart/domain/repositories/cart_repository.dart
@@ -0,0 +1,9 @@
+// Abstract class: kontrak yang harus diikuti oleh implementasi
+// Prinsip Dependency Inversion (DIP) dari SOLID
+
+abstract class CartRepository {
+  List<Product> getCartItems();
+  void addItem(Product product);
+  void removeAllItems();
+  bool isItemInCart(String productId);
+}
