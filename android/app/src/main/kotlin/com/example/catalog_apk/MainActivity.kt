d045e9522da6fd2a858ab93543507434fa1f6716
Ferdiansyah76 <bagusferdiansyah750@gmail.com>
2026-03-05 22:12:01 +0700

    menambahkan kata kata

---
+++ android/app/src/main/kotlin/com/example/catalog_apk/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.catalog_apk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main () {
+  runApp(
+    // Membungkus aplikasi dengan ChangeNotifierProvider
+    // agar State bisa diakses di mana saja
+  );
+}
